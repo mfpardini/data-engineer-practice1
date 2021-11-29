2541c1966cd9b4cddc787786dba0285639e2bb82
mfpardini <matheusfpardini@gmail.com>
2021-11-28 21:07:57 -0300

    insert on vehicle_status table

---
+++ insert_vehicle_status.sql
@@ -0,0 +1,3 @@
+INSERT INTO relational.vehicle_status VALUES (1, 'avaliable');
+INSERT INTO relational.vehicle_status VALUES (2, 'unavailable');
+INSERT INTO relational.vehicle_status VALUES (3, 'rented');
